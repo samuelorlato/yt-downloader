217ba7207964069d80b36f19d50492b239adcf2c
samuel <samuelorlato@gmail.com>
2021-04-15 20:59:29 -0300

    correction in download

--- public/download.js
+++ public/download.js
@@ -3,9 +3,9 @@ document.getElementById("download-video").addEventListener("click", function(e){
     e.preventDefault()
     var select = document.getElementById("format-opt")
     if(select.options[select.selectedIndex].text === "MP3"){
-        window.open(window.location.href+"/downloadMP3?url="+url)
+        window.open("https://yt-videos-downloader.herokuapp.com/downloadMP3?url="+url)
     }
     else if(select.options[select.selectedIndex].text === "MP4"){
-        window.open(window.location.href+"/downloadMP4?url="+url)
+        window.open("https://yt-videos-downloader.herokuapp.com/downloadMP4?url="+url)
     }
 })
